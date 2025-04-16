c5a4e56c3305b86efc194536f92083effab392ad
Your Name <you@example.com>
2025-04-16 12:27:21 +0700

    second commit

--- client/src/components/form/TextAreaInput.jsx
+++ client/src/components/form/TextAreaInput.jsx
@@ -18,25 +18,3 @@ const TextAreaInput = ({ register, name, placeholder }) => {
 };
 
 export default TextAreaInput;
-
-// import React from "react";
-// import { Label } from "../ui/label";
-// import { Textarea } from "../ui/textarea";
-
-// const TextAreaInput = ({ register, name, type, placeholder }) => {
-//   return (
-//     <div className="mb-2">
-//       <Label htmlFor={name} className="capitalize">
-//         {name}
-//       </Label>
-//       <Textarea
-//         {...register(name)}
-//         className="min-h-[7rem]"
-//         type={type}
-//         placeholder={placeholder}
-//       />
-//     </div>
-//   );
-// };
-
-// export default TextAreaInput;
